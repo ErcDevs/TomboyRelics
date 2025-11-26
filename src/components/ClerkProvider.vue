93334b764f4da968e13ec577da8b017bbda49e50
ErcDevs <122711566+ErcDevs@users.noreply.github.com>
2025-11-25 23:56:46 -0700

    added clerk provider for customer accounts

---
+++ src/components/ClerkProvider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div ref="clerkEl"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import { Clerk } from '@clerk/clerk-js'
+
+const clerkEl = ref(null)
+
+onMounted(() => {
+  const clerk = new Clerk('pk_test_cHJlY2lvdXMtYm9hci01My5jbGVyay5hY2NvdW50cy5kZXYk')  // ← get from Clerk dashboard
+  clerk.load({}).then(() => {
+    clerk.mountUserButton(clerkEl.value)
+  })
+})
+</script>
