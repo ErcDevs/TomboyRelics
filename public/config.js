af609087aea8f2011d2d2613ca8ec7b8e3479877
ErcDevs <122711566+ErcDevs@users.noreply.github.com>
2025-11-25 22:42:01 -0700

    config for square

---
+++ public/config.js
@@ -0,0 +1,4 @@
+window.SQUARE_CONFIG = {
+  applicationId: "sq0idp-Bw2OZrLNREC4xHwCJ0AWvQ",  // Paste from Developer Console
+  locationId: "L93AFY32TSKHG"        // Paste from Developer Console
+};
